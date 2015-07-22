976eaac1913a0fed762cd252eb8970cb4eb8b173
mjwweber <marajwweber@gmail.com>
2015-07-22 16:27:59 -0400

    Adding Validation
    Added validation to the movie class to put restrictions on what can be
    entered in the various sections of the Create New Movie function on the
    site.

--- McvMovie/Models/Movies.cs
+++ McvMovie/Models/Movies.cs
@@ -11,14 +11,26 @@ namespace McvMovie.Models
     public class Movies
     {
         public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)] //added 7/22
         public string Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")] //added 7/22
+        [Required] //added 7/22
+        [StringLength(30)] //added 7/22
         public string Genre { get; set; }
+
+        [Range(1, 100)]  //added 7/22
+        [DataType(DataType.Currency)] //added 7/22
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")] //added 7/22
+        [StringLength(5)] //7/22
         public string Rating { get; set; }
     }
 
