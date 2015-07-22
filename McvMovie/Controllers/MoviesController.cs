41a7c1abd5a0733c900c1d788c861f7b843b7ae6
mjwweber <marajwweber@gmail.com>
2015-07-22 14:29:33 -0400

    Adding a New Field
    Using migrations to add the Rating field to the movie details.

--- McvMovie/Controllers/MoviesController.cs
+++ McvMovie/Controllers/MoviesController.cs
@@ -67,7 +67,7 @@ namespace McvMovie.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movies movies)
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movies movies)
         {
             if (ModelState.IsValid)
             {
@@ -99,7 +99,7 @@ namespace McvMovie.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movies movies)
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movies movies)
         {
             if (ModelState.IsValid)
             {
